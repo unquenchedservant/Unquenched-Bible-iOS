18110fd21f9c750a07e166029925fdb6f88e5760
Jonathan Thorne <contact@theunquenchedservant.com>
2020-12-29 19:05:00 -0500

    Initial Commit

---
+++ Unquenched Bible/Unquenched_BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unquenched_BibleApp.swift
+//  Unquenched Bible
+//
+//  Created by Jonathan Thorne on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Unquenched_BibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
